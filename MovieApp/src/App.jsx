2dcc3a4a5454bb927ba5662242c251173d2b8b81
ahmed-qureashi <ahmed@neusco.com>
2025-08-24 17:51:42 +0500

    first commit

---
+++ MovieApp/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = ({ title}) => {
+
+
+  const [hasliked, setHasliked] = useState(false)
+  return(
+    <div className='card' >
+     <h2>{title}</h2>
+     <button onClick={() => setHasliked(true)}>
+      {hasliked ? '❤️' : '🤍'}
+     </button>
+     
+    </div>
+  )
+}
+
+const  App = () => {
+
+  return(
+    <div> 
+      <div className="card-container"></div>
+      <Card title="Inception" rating={0.5} actors={[{name:'Actors'}]}/>
+      <Card title="Interstellar"/>
+      <Card title="The Dark Knight"/>
+     
+    </div>
+  )
+  
+}
+export default App
